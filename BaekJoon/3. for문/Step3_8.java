e4282518209901026ed43d88f4beb846a3870a26
kwangyunwon <dnjs2rhkddus@naver.com>
2021-03-16 17:25:52 +0900

    update

---
+++ BaekJoon/3. for문/Step3_8.java
@@ -0,0 +1,23 @@
+package three;
+
+import java.util.Scanner;
+
+/*
+ * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ */
+public class Step3_8 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
