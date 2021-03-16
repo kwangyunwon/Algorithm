b7cf68d08e3dffaa67b81205f1dbaeed9f35a4f1
kwangyunwon <dnjs2rhkddus@naver.com>
2021-03-16 17:17:33 +0900

    update

---
+++ BaekJoon/3. for문/Step3_7.java
@@ -0,0 +1,23 @@
+package three;
+
+import java.util.Scanner;
+
+/*
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+public class Step3_7 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int a, b;
+		
+		for(int i = 1 ; i <= N; i++) {
+			a = scan.nextInt();
+			b = scan.nextInt();
+			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
+		}
+	}
+
+}
